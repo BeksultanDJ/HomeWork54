d25d6f0c8156a3c3965ffc0edd2dae79e292f79a
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-10-30 16:25:54 +0600

    Этот файл создает доску 36х36 и соединяет все файлы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import './App.css'
+import React, { useState } from "react";
+import Board from "./Board";
+import ResetButton from "./ResetButton";
+import './App.css';
 
-function App() {
-
-
-  return (
-    <>
-
-    </>
-  )
+function createItems(): boolean[] {
+    const items: boolean[] = Array(36).fill(false);
+    const randomIndex = Math.floor(Math.random() * 36);
+    items[randomIndex] = true;
+    return items;
 }
 
-export default App
+function App() {
+    const [items, setItems] = useState(createItems());
+    const [attempts, setAttempts] = useState(0);
+    const [isGameWon, setIsGameWon] = useState(false);
+
+    const resetGame = () => {
+        setItems(createItems());
+        setAttempts(0);
+        setIsGameWon(false);
+    };
+
+    const handleCellClick = (index: number) => {
+        if (items[index]) {
+            setIsGameWon(true);
+        } else {
+            setAttempts(attempts + 1);
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Найди кольцо</h1>
+            <p>Попыток: {attempts}</p>
+            {isGameWon ? <div className="game-won">Вы победили!</div> : null}
+            <ResetButton onReset={resetGame} />
+            <Board items={items} onCellClick={handleCellClick} />
+        </div>
+    );
+}
+
+export default App;
